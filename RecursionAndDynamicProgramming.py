667bf2fe7e150ada6dbcb108ff13671b02c893a5
Keren Ben Arosh <kerenb20@gmail.com>
2020-03-01 01:03:11 +0200

    8.3 magic_index

--- RecursionAndDynamicProgramming.py
+++ RecursionAndDynamicProgramming.py
@@ -62,6 +62,49 @@ def triple_steps(steps):
     return d
 
 
-print(triple_step_rec(5))
-triple_steps_mem(5)
-triple_steps(5)
+# print(triple_step_rec(5))
+# triple_steps_mem(5)
+# triple_steps(5)
+
+
+def find_mi(ints, min_idx, max_idx):
+    """8.3 helper"""
+    print(f'min: {min_idx}, max: {max_idx}')
+    if min_idx > max_idx:
+        return -1
+
+    mid_idx = (min_idx + max_idx) // 2
+    mid_val = ints[mid_idx]
+    if mid_idx == mid_val:
+        return mid_idx
+
+    # search left
+    left_idx = min(mid_idx-1, mid_val)
+    left = find_mi(ints, min_idx, left_idx)
+    if left > -1:
+        return left
+
+    # search right
+    right_idx = max(mid_idx+1, mid_val)
+    return find_mi(ints, right_idx, max_idx)
+
+    # if mid_idx < ints[mid_idx]:
+    #     down = find_mi(ints, min_idx, mid_idx - 1)
+    #     if down > -1:
+    #         return down
+    #     return find_mi(ints, ints[mid_idx], max_idx)
+    # else:
+    #     up = find_mi(ints, mid_idx + 1, max_idx)
+    #     if up > -1:
+    #         return up
+    #     return find_mi(ints, min_idx, ints[mid_idx])
+
+
+def magic_index(ints):
+    """8.3"""
+    if ints is None or len(ints) == 0:
+        return -1
+    return find_mi(ints, 0, len(ints) - 1)
+
+
+print(magic_index([2,2,3,4,6,6,6,8]))
